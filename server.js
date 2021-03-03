33f2da7b4a0dac5b20141b76e033e42d923268dd
Renan Peres <renan.peres@sys3sistemas.com.br>
2021-03-03 10:57:22 -0300

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(__dirname + '/dist/<name-of-app>'));
+app.use(express.static(__dirname + '/dist/sexlog-test'));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname+'/dist/<name-of-app>/index.html'));
+  res.sendFile(path.join(__dirname+'/dist/sexlog-test/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
